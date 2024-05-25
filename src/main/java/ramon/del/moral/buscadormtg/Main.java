4c87fb18a2f52407e3d707944b0fd8e8c08e2410
ramon <rpuntodma@gmail.com>
2024-05-25 02:25:56 +0200

    Spring JPA persistance works and structure layers

--- src/main/java/ramon/del/moral/buscadormtg/Main.java
+++ src/main/java/ramon/del/moral/buscadormtg/Main.java
@@ -4,31 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class Main extends Application {
 
     private ConfigurableApplicationContext context;
 
     @Override
     public void init() {
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(ProjectSpringBootApp.class);
-        context = builder.run(getParameters().getRaw().toArray(new String[0]));
+//        SpringApplicationBuilder builder = new SpringApplicationBuilder(ProjectSpringBootApp.class);
+//        context = builder.run(getParameters().getRaw().toArray(new String[0]));
+        context = SpringApplication.run(ProjectSpringBootApp.class);
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        fxmlLoader.setControllerFactory(context::getBean);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Buscador!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void stop() {
+        context.close();
     }
 }
---
+++ src/main/java/ramon/del/moral/buscadormtg/services/impl/DefaultCardServiceImpl.java
@@ -0,0 +1,37 @@
+package ramon.del.moral.buscadormtg.services.impl;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+import ramon.del.moral.buscadormtg.daos.CardDao;
+import ramon.del.moral.buscadormtg.entities.CardModel;
+import ramon.del.moral.buscadormtg.services.CardService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DefaultCardServiceImpl implements CardService {
+
+    @Resource
+    private CardDao cardDao;
+
+    @Override
+    public List<CardModel> findAll() {
+        return cardDao.findAll();
+    }
+
+    @Override
+    public Optional<CardModel> findById(Long id) {
+        return cardDao.findById(id);
+    }
+
+    @Override
+    public CardModel save(CardModel card) {
+        return cardDao.save(card);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        cardDao.deleteById(id);
+    }
+}
